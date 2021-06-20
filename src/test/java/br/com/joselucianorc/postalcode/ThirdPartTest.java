89bd1d08d7fe537085a71b4f06a9907b10f875dc
José Luciano Julietti Rodrigues de Castro <joselucianorc@gmail.com>
2021-06-19 22:44:59 -0300

    Testing third part mocked api

---
+++ src/test/java/br/com/joselucianorc/postalcode/ThirdPartTest.java
@@ -0,0 +1,29 @@
+package br.com.joselucianorc.postalcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@ExtendWith(MockitoExtension.class)
+public class ThirdPartTest {
+	
+	final String thirdPartUrl = "https://api.publicapis.org/entries?category=animals&https=true&Cors=unknow&Auth=apiKey"; 
+	
+	@Test
+	public void testThirdPartApi() throws URISyntaxException {
+		RestTemplate restTemplate = new RestTemplate();
+		URI uri = new URI(thirdPartUrl);
+
+		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+		assertEquals(200, result.getStatusCodeValue());
+		assertTrue(result.getBody().contains("https://docs.thecatapi.com"));
+	}
+}
